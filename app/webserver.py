9355c58e0452a3eb17069994dff715b895215805
Sascha Leyer <sascha.leyer@coremedia.com>
2022-08-29 17:42:49 +0200

    Added logging.

--- app/webserver.py
+++ app/webserver.py
@@ -1,4 +1,5 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
 
 hostName = "localhost"
 serverPort = 8080
@@ -11,9 +12,23 @@ class HelloWorldServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(self.response_data, "utf-8"))
 
+def get_module_logger(mod_name):
+    """
+    To use this, do logger = get_module_logger(__name__)
+    """
+    logger = logging.getLogger(mod_name)
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+        '%(asctime)s [%(name)-12s] %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
+    return logger
+
 if __name__ == "__main__":
+    _logger = get_module_logger(__name__)
     webServer = HTTPServer((hostName, serverPort), HelloWorldServer)
-    print(f"Server started at http://{hostName}:{serverPort}")
+    _logger.info(f"Server started at http://{hostName}:{serverPort}")
 
     try:
         webServer.serve_forever()
